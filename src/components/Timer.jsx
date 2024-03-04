45c0fe6618a0fafe349930f1fdd18775cc498bb1
kvisten-e <kvist.emil@gmail.com>
2024-03-04 15:47:00 +0100

    Ändrat timer ifrån UTC till CET. Både i kod Timer.jsx och endTime under db.json

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -32,7 +32,7 @@ export default function Timer(props) {
   };
 
   return <>
-    {timeCloseAution ? <p style={styleClosed}>Auktion avslutad</p> : time != null ? <div style={styleTimer}><Countdown date={Date.parse(time)} onComplete={Completionist} renderer={renderer}></Countdown></div>
+    {timeCloseAution ? <p style={styleClosed}>Auktion avslutad</p> : time != null ? <div style={styleTimer}><Countdown date={Date.parse(new Date(time).toLocaleString('se-SE', { timeZone: 'cet' }))} onComplete={Completionist} renderer={renderer}></Countdown></div>
       : <p>00:00:00:00</p>}
   </>
 
