1a5d06558a3fc2373d5db7002b2d31ecb0fc1973
kvisten-e <kvist.emil@gmail.com>
2024-03-04 09:52:59 +0100

    Lagt till Carousel

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+
+export default function Carousel(props) {
+
+  const [slide, setSlide] = useState(0);
+
+  let images = props.objImages
+  const width = props.width
+  const height = props.height
+
+
+  const nextSlide = () => {
+    setSlide(slide === images.length - 1 ? 0 : slide + 1);
+  }
+
+  const prevtSlide = () => {
+    setSlide(slide === 0 ? images.length - 1 : slide - 1);
+  }
+
+  const sizeCarousel = {
+    width: width,
+    height: height
+  }
+
+  return <>
+    {images != null ?
+      <div className="image-container" style={sizeCarousel}>
+        <div className="carousel">
+          {props.objImages.map((image, idx) => {
+            return (
+              <img className={slide === idx ? "slide" : "slide-hidden"} src={image} key={idx} alt={'https://media0.giphy.com/media/3oEjI6SIIHBdRxXI40/200w.gif?cid=6c09b952h9gko1rwe3vohc2q8oucwami901vcwetrds7e5ti&ep=v1_gifs_search&rid=200w.gif&ct=g'} />
+            )
+          })}
+          <span className="indicators">
+            {images.map((_, idx) => {
+              return <button key={idx} onClick={() => setSlide(idx)} className={slide === idx ? "indicator" : "indicator indicator-inactive"}></button>
+            })}
+          </span>
+        </div>
+        <BsArrowLeftCircleFill className="arrow arrow-left" onClick={prevtSlide} />
+        <BsArrowRightCircleFill className="arrow arrow-right" onClick={nextSlide} />
+      </div >
+      : <div className="image-container" style={sizeCarousel}><p>No images found</p></div>
+    }
+
+  </>
+}
+
