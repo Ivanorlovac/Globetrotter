069823231e06ce8f77cd1b9f79fb1f37f0badfe0
Juliakronm <juliakronmarker@gmail.com>
2024-03-05 13:35:23 +0100

    lagt in font och knapp

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -4,41 +4,51 @@ import Col from 'react-bootstrap/Col';
 import AuktionsLista from '../components/AuktionsLista';
 
 export default function Homepage() {
-  return <div id='homepage_first_section'>
+  return <>
+  
+  <div id='homepage_first_section'>
     <div id='hompage_box'>
       <div>
-        <div>
-        <h2>Sök din resa</h2>
+        <div className='center'>
+        <h2>HITTA DIN NÄSTA RESA</h2>
         </div>
       </div>
-      <div>
-        <div>
+      <div id='homepage_search'>
+        <div id='sec_one'>
           <div>
-            <div><p>Fritext</p></div>
+            <p>Fritext</p>
           </div>
           <div>
             <div>Sökruta</div>
           </div>
         </div>
-        <div>Eller</div>
-        <div>
+        <div id='sec_two'>eller</div>
+        <div id='sec_three'>
           <div>
-            <div>Välj datum</div>
+            <p>Välj datum</p>
           </div>
           <div>
             <div>Kalender</div>
           </div>
         </div>
       </div>
-      <div>
-        <div><button>Shuffle</button></div>
+      <div id='shuffle_section'>
+        <div className='center'><button id='shuffle_button'><span class="material-symbols-outlined">
+          shuffle
+        </span><br/>SLUMPA</button></div>
       </div>
 
     </div>
   </div>
 
+    <div id='homepage_second_section'>
+      <div className='blue_box_centered'>
+        <AuktionsLista />
+      </div>
+  </div>
 
 
 
+  </>
 
 }
