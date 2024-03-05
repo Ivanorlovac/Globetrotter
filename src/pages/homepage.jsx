e61c71809bb294feb49582b96df98f0aa6aebe3c
Juliakronm <juliakronmarker@gmail.com>
2024-03-05 12:08:29 +0100

    Påbörjad design till startsida

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -4,13 +4,37 @@ import Col from 'react-bootstrap/Col';
 import AuktionsLista from '../components/AuktionsLista';
 
 export default function Homepage() {
-  return <div>
-    <Container>
-      <Row>
-        <Col>Här är text</Col>
-        <AuktionsLista />
-      </Row>
-    </Container>
+  return <div id='homepage_first_section'>
+    <div id='hompage_box'>
+      <div>
+        <div>
+        <h2>Sök din resa</h2>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <div><p>Fritext</p></div>
+          </div>
+          <div>
+            <div>Sökruta</div>
+          </div>
+        </div>
+        <div>Eller</div>
+        <div>
+          <div>
+            <div>Välj datum</div>
+          </div>
+          <div>
+            <div>Kalender</div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div><button>Shuffle</button></div>
+      </div>
+
+    </div>
   </div>
 
 
