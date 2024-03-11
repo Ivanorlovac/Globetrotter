4925ccd93ccc6cf18644b9c956d19cbcda1d5fa6
kvisten-e <kvist.emil@gmail.com>
2024-03-11 14:18:24 +0100

    Börjat mappa ut favoriter

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,10 +1,17 @@
 import { useContext, useState, useEffect } from "react"
 import { Globalcontext } from "../components/GlobalContext.jsx"
+import Carousel from "../components/Carousel.jsx";
+import Timer from "../components/Timer.jsx";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+
+
+
 export default function MyPage() {
 
   const { favorites } = useContext(Globalcontext)
-  const [showFavorites, setShowFavorites] = useState(false);
+  const [showFavorites, setShowFavorites] = useState(true);
   const [filteredAuctions, setFilteredAuctions] = useState([]);
+
 
 
   const favoritesPopdown = () => {
@@ -52,10 +59,34 @@ export default function MyPage() {
         setFilteredAuctions(data);
       })
       .catch(error => console.error('Fel:', error));
-  }, []);
+  }, [favorites]);
 
 
   console.log("Favorites: ", favorites)
+
+  const ShowFavorites = () => {
+    return <>
+      {filteredAuctions.map(favorite => (
+        <article className="favorite-article">
+          <div className="favorite-picture">
+            <Carousel objImages={favorite.images} />
+          </div>
+          <div className="favorite-information">
+            <h3>{favorite.title}</h3>
+            <p>Kategori: {favorite.category}</p>
+            <p>Värdering pris: {favorite.valuationPrice}</p>
+            <div>
+              <p>Sluttid: </p>
+              <Timer objEndTime={favorite.endTime} fontSize={10} showBorder={false} setBold={true} />
+            </div>
+          </div>
+          <div className="favorite-my-bid">
+
+          </div>
+        </article>
+    ))}
+    </>
+  }
 
   return <>
 
@@ -65,11 +96,12 @@ export default function MyPage() {
       </section>
       <section>
         <div className="favorite-section">
-          <h3 className="faqQuestion" onClick={favoritesPopdown}>My favorites</h3>
-          {!showFavorites && <p className="show-favorites"> heej</p>}
+          <h3 className="favorite-drop-button" onClick={favoritesPopdown}>My favorites</h3>
+          {showFavorites ? <IoMdArrowDropdown style={{ fontSize: "30px" }} /> : <IoMdArrowDropup style={{ fontSize: "30px" }} />}
         </div>
+          {!showFavorites && <ShowFavorites />}
         <div className="my-bids-section">
-
+ 
         </div>
       </section>
     </div>
