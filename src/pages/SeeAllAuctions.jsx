a0d37029d68b334598b64ee2f7dc2b5cb9cd36ed
kvisten-e <kvist.emil@gmail.com>
2024-03-11 14:38:23 +0100

    Knapp för att scrolla till toppen av sidan syns bara i slutet

--- src/pages/SeeAllAuctions.jsx
+++ src/pages/SeeAllAuctions.jsx
@@ -1,15 +1,33 @@
 import { AuktionsLista } from "../components/AuktionsLista";
 import ScrollToTopButton from "../components/ScrollToTop";
 
-
+import { useEffect, useState } from "react";
 
 
 export default function SeeAll() {
 
+  const [scrollUp, setScrollUp] = useState(false)
+
+  useEffect(() => {
+    const onScroll = () => {
+      const scrolledTo = window.scrollY + window.innerHeight + 500;
+
+      if (document.body.scrollHeight < scrolledTo) {
+        setScrollUp(true)
+      } else {
+        setScrollUp(false)
+      }
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
   return <div id="see_all_auctions">
     <div id="blue_background_box">
       <AuktionsLista />
-      <ScrollToTopButton/>
+      {scrollUp ? <ScrollToTopButton/> : <></>}
     </div>
   </div>
 
