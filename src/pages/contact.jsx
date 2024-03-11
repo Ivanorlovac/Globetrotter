25e50ce0f732f04cfd4f7a239624d8037969890a
Vanessa R. <vanessasweden94@outlook.com>
2024-03-11 17:08:25 +0100

    deleted the contact.css file because something with the import is not working. I will try to include the css in the contact.jsx file instead.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,48 @@
+import { Container, Row, Col, Button} from 'react-bootstrap';
+
+
 function Contact() {
   return (
-    <p>hi</p>
+    <Container>
+      <Row>
+        <Col>
+          <h1 className="header">Kontakta oss</h1>
+          <form>
+            <div className="form-group">
+              <label htmlFor="firstName">Förnamn</label>
+              <input className="custom-input" type="text" id="firstName"/>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">E-post</label>
+              <input className="custom-input" type="email"  id="email"/>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="phone">Telefonnummer</label>
+               <input className="custom-input" type="tel" id="phone"/>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="message">Meddelande</label>
+              <textarea className="custom-input" id="message" rows={10}></textarea>
+            </div>
+              
+            <Button className="button" variant="primary" type="submit">Skicka</Button>
+          </form>    
+        </Col>
+
+        <Col>
+          <div className="contact-info">
+            <h2 className="title">Kontaktinformation</h2>
+            <p>E-post: travelauction.globetrotter@gmail.com</p>
+            <p>Address: Propellergatan 1, 211 15 Malmö</p>
+            <p>Telefon: 076 057 788 2</p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
-export default Contact
+export default Contact;
