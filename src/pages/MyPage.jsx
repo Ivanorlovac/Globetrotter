8fab78d7e466684578bd8cb8cbb2131a0530b2c4
kvisten-e <kvist.emil@gmail.com>
2024-03-12 12:39:32 +0100

    Fixat så man kan se alla ens auktioner

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -11,15 +11,68 @@ import { IoIosArrowForward } from "react-icons/io";
 
 export default function MyPage() {
 
-  const { favorites, setFavorites } = useContext(Globalcontext)
+  const { favorites, setFavorites, user } = useContext(Globalcontext)
   const [showFavorites, setShowFavorites] = useState(true);
   const [showAuctions, setShowAuctions] = useState(true);
   const [showEndedAuctions, setShowEndedAuctions] = useState(true);
-
-
+  const [myAuctions, setMyAuctions] = useState([])
+  const [myBids, setMybids] = useState([])
   const [filteredFavoriteAuctions, setFilteredFavoriteAuctions] = useState([]);
 
 
+  useEffect(() => {
+    async function load() {
+      const id = user.id
+
+      async function getMyBids() {
+        const response = await fetch('/api/bids?userId=' + id)
+        const data = await response.json()
+        return data
+      }
+      async function getAuctions() {
+        const response = await fetch('/api/auctions')
+        const data = await response.json()
+        return data 
+      }
+
+      const dataBids = await getMyBids()
+      const dataAuctions = await getAuctions()
+
+      if (dataBids && dataAuctions) {
+
+        let myAuctionsData = dataAuctions.filter(auction => {
+          return dataBids.some(bid => {
+            return bid.auctionId === auction.id
+          })
+        })
+
+        setMyAuctions(myAuctionsData)
+
+        let data = dataAuctions.filter(auction => {
+          let timeNow = new Date().toLocaleString('se-SE', { timeZone: 'cet' })
+          let timeEnd = new Date(auction.endTime).toLocaleString('se-SE', { timeZone: 'cet' })
+          return timeNow < timeEnd
+        })
+
+        data = data.sort((a, b) => {
+          return new Date(a.endTime) - new Date(b.endTime)
+        })
+
+        data = data.filter(auction => {
+          return favorites.some(favorite => {
+            return favorite.auction_id === auction.id
+          })
+        })
+
+        setFilteredFavoriteAuctions(data);           
+
+      }
+      
+    }
+    load()
+
+
+  }, [])
 
   const favoritesPopdown = () => {
     if (showFavorites) {
@@ -43,42 +96,6 @@ export default function MyPage() {
     }
   };  
 
-  useEffect(() => {
-
-    if (!favorites) {
-      return
-    }
-
-    fetch('http://localhost:3000/auctions')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Något gick fel vid hämtning av auktioner');
-        }
-        return response.json();
-      })
-      .then(data => {
-
-        data = data.filter(auction => {
-          let timeNow = new Date().toLocaleString('se-SE', { timeZone: 'cet' })
-          let timeEnd = new Date(auction.endTime).toLocaleString('se-SE', { timeZone: 'cet' })
-          return timeNow < timeEnd
-        })
-
-        data = data.sort((a, b) => {
-          return new Date(a.endTime) - new Date(b.endTime)
-        })
-
-        data = data.filter(auction => {
-          return favorites.some(favorite => {
-            return favorite.auction_id === auction.id
-          })
-        })
-
-        console.log("Data ny: ", data)
-        setFilteredFavoriteAuctions(data);
-      })
-      .catch(error => console.error('Fel:', error));
-  }, [favorites]);
 
   const toggleFavorite = (articleObj) => {
     const existingFavorite = favorites.find(obj => obj.auction_id === articleObj.id);
@@ -131,7 +148,33 @@ export default function MyPage() {
 
   const ShowAuctions = () => {
     return <>
-      <p>Hej</p>
+      {
+        myAuctions.map(auction => (
+          <article className="favorite-article">
+            <div className="favorite-picture">
+              <Carousel objImages={auction.images} />
+            </div>
+            <div className="favorite-information">
+              <h3>{auction.title}</h3>
+              <p>Kategori: {auction.category}</p>
+              <p>Värdering pris: {auction.valuationPrice}</p>
+              <div>
+                <p>Sluttid: </p>
+                <Timer objEndTime={auction.endTime} fontSize={10} showBorder={false} setBold={true} />
+              </div>
+            </div>
+            <div className="favorite-star">
+              <h3>Mitt bud: </h3>
+              <p>200</p>
+            </div>
+            <div className="favorite-go-to-page">
+              <Link to={`/auction/${auction.id}/${auction.slug}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                <IoIosArrowForward style={{ fontSize: "45px" }} />
+              </Link>
+            </div>
+          </article>
+        ))
+      }
     </>
   }
 
@@ -153,11 +196,13 @@ export default function MyPage() {
           {showFavorites ? <IoMdArrowDropdown style={{ fontSize: "30px" }} /> : <IoMdArrowDropup style={{ fontSize: "30px" }} />}
         </div>
         {!showFavorites && <ShowFavorites />}
+
         <div className="auctions-section" onClick={auctionsPopdown}>
           <h3 className="auctions-drop-button" >Aktiva auktioner</h3>
           {showAuctions ? <IoMdArrowDropdown style={{ fontSize: "30px" }} /> : <IoMdArrowDropup style={{ fontSize: "30px" }} />}
         </div>
         {!showAuctions && <ShowAuctions />}
+        
         <div className="ended-auctions-section" onClick={endedAuctionsPopdown}>
           <h3 className="ended-auctions-drop-button" >Avslutade auktioner</h3>
           {showEndedAuctions ? <IoMdArrowDropdown style={{ fontSize: "30px" }} /> : <IoMdArrowDropup style={{ fontSize: "30px" }} />}
