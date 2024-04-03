289b04da865907ebf9c0279aa090d4afdd21183d
Ivan Orlovac <ivanorlovac@gmail.com>
2024-04-03 14:01:43 +0200

    Seeall and SeeOne

---
+++ Server/AuctionId.cs
@@ -0,0 +1,9 @@
+// namespace Server;
+
+//     public class Auction
+// {
+//     public string Id { get; set; }
+//     public string Title { get; set; }
+ 
+
+// }
--- Server/Program.cs
+++ Server/Program.cs
@@ -4,5 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/auctions", Auctions.All);
+app.MapGet("/auctions/{auctionsId}", (string auctionsId) => Auctions.GetAuctionById(auctionsId));
+
+
+
+
 
 app.Run();
