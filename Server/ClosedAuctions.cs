ae54576b85712f7fe727fd768253bdf9385dcd8d
kvisten-e <kvist.emil@gmail.com>
2024-04-10 10:11:09 +0200

    Skrivit som så att endpointens värden är identiska till våran db.json

--- Server/ClosedAuctions.cs
+++ Server/ClosedAuctions.cs
@@ -6,7 +6,21 @@ namespace Server;
 
 public class ClosedAuctions
 {
-    public record ClosedAuction(int? auctionId = null, int? winner = null, int? amount = null);
+    public record ClosedAuction(string id, 
+                                string title, 
+                                string slug, 
+                                string description, 
+                                int valuationPrice, 
+                                int priceRange,
+                                List<string> images,
+                                DateTime endTime,
+                                string category,
+                                string? winner_user_id = null,
+                                int? winner_amount = null,
+                                string? creator = null,
+                                string? creatorImage = null
+                                );
+
     public static List<ClosedAuction> GetAllClosedAuctions(State state)
     {
         List<ClosedAuction> result = new();
@@ -14,21 +28,55 @@ public class ClosedAuctions
 
         var reader = MySqlHelper.ExecuteReader(
             state.DB,
-            "SELECT * FROM ClosedAuctions"
+            @"SELECT a.id, a.title, a.slug, a.description, a.valuationPrice, a.priceRange, a.images, a.endTime, cat.name as category, ca.winner as winner_user_id, ca.amount as winner_amount, c.companyName as creator, c.logo as creatorImage FROM Auctions as a
+                    LEFT JOIN ClosedAuctions ca ON a.id = ca.auctionId
+                    LEFT JOIN Companies c ON a.company = c.id
+                    LEFT JOIN Categories cat ON cat.id = a.category;"
         );
 
         while (reader.Read())
         {
+            var imagesArray = reader.GetString("images")
+                                    .Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(img => img.Trim() 
+                                                      .Trim(new char[] { '\"', '\r', '\n' })
+                                                      )
+                                    .ToList();
             try
             {
-                result.Add(new(reader.GetInt32("auctionId"), reader.GetInt32("winner"), reader.GetInt32("amount")));
+                result.Add(new(
+                    Convert.ToString(reader.GetInt32("id")),
+                    reader.GetString("title"),
+                    reader.GetString("slug"),
+                    reader.GetString("description"),
+                    reader.GetInt32("valuationPrice"),
+                    reader.GetInt32("priceRange"),
+                    imagesArray,
+                    reader.GetDateTime("endTime"),
+                    reader.GetString("category"),
+                    Convert.ToString(reader.GetInt32("winner_user_id")),
+                    reader.GetInt32("winner_amount"),
+                    reader.GetString("creator"),
+                    reader.GetString("creatorImage")));
             }
             catch
             {
-                result.Add(new(reader.GetInt32("auctionId"), null, null));
+                result.Add(new(
+                    Convert.ToString(reader.GetInt32("id")),
+                    reader.GetString("title"),
+                    reader.GetString("slug"),
+                    reader.GetString("description"),
+                    reader.GetInt32("valuationPrice"),
+                    reader.GetInt32("priceRange"),
+                    imagesArray,
+                    reader.GetDateTime("endTime"),
+                    reader.GetString("category"),
+                    null,
+                    null,
+                    reader.GetString("creator"),
+                    reader.GetString("creatorImage")));
             }
         }
-
 
         return result;
     }
