5c54427185da17d731d566b1d019546115bebc61
kvisten-e <kvist.emil@gmail.com>
2024-05-02 11:29:07 +0200

    Fixat nav och login features

--- cypress/specs/example.feature
+++ cypress/specs/example.feature
@@ -2,3 +2,6 @@ Feature: Test site
 
     Scenario: Test home page
         Given that I am on home page
+
+
+        
---
+++ cypress/specs/step_definitions/login.js
@@ -0,0 +1,37 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+
+Then('I get directed to the login page', () => {
+  cy.get('h1').should("have.text", "Logga in")
+});
+
+
+When('I write my {string} under username', (username) => {
+  cy.get('#username').type(username)
+});
+
+When('I write my {string} under password', (password) => {
+  cy.get('#password').type(password)
+});
+
+Then('I will get logged in', () => {
+  cy.get("#navbar-button").click()
+  cy.get("#navlink-logout").should("exist")
+});
+
+Then('I will not get logged in {string} and {string}', (username, password) => {
+  cy.request({
+    method: 'POST',
+    url: 'http://localhost:5173/api/login',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: {
+      username: username,
+      password: password
+    },
+    failOnStatusCode: false
+  }).then((resp) => {
+    expect(resp.status).to.eq(500);
+  });
+});
