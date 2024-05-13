5df86104a8e3f8981376b6f0dfc81c743aea2d43
kvisten-e <kvist.emil@gmail.com>
2024-05-13 14:08:21 +0200

    Ändrat order på tester

--- cypress/support/runSql.js
+++ cypress/support/runSql.js
@@ -3,7 +3,7 @@ const mysql2 = require('mysql2/promise');
 const pool = mysql2.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'mypassword',
+    password: 'Dunder123!1',
     database: 'Globetrotter',
     waitForConnections: true,
     connectionLimit: 10,
