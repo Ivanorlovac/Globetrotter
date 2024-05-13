d44d9e6dd14d66da742734b2d3439c7cc9d12ed3
kvisten-e <kvist.emil@gmail.com>
2024-05-13 10:46:51 +0200

    Fixat bidding testing

---
+++ cypress/specs/step_definitions/bidding.js
@@ -0,0 +1,33 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+
+When('I place a bid', () => {
+  cy.get('.input-place-bid').type(10000)
+});
+
+Then('the bid placed alert will pop up', () => {
+  cy.get('.fade').should("be.visible")
+});
+
+Then('I can see my new bid', () => {
+  cy.get('p > span').should("have.text", "10000 kr")
+});
+
+
+When('I have already a bid on the auction', () => {
+  cy.get('#navbar-button').click()
+  cy.get('#navlink-my-page').click()
+  cy.get('.btn-close').click()
+  cy.get('.auctions-section').click()
+  cy.get('.favorite-article').should('be.visible')
+  cy.get('a > svg').click()
+});
+
+When('I place a lower bid than my current', () => {
+  cy.get('.input-place-bid').type(9000)
+});
+
+
+Then('the false bid alert will show up.', () => {
+  cy.get('.fade').should("be.visible")  
+});
